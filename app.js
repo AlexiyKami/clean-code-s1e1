e2bc4d43891926ec127c3b1fcef3a131f1e657f6
AlexiyKami <Leksij01@gmail.com>
2022-05-28 19:10:02 +0300

    fix: add alt for images according to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="delete-button";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className = "remove-img";
+    deleteButtonImg.alt='Remove button';
     deleteButton.appendChild(deleteButtonImg);
 
 
